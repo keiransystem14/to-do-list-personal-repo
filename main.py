97b39588734d0a20e9292c3a17d34de1b2ba81da
keiransystem14 <keiran1412@hotmail.com>
2023-03-08 06:41:53 +0000

    First write up of code on my personal to-do-list application

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title("To-Do-List")
+root.geometry("400x650x400+100")
+root.resizeable(False,False)
+
+root.mainloop()
+
