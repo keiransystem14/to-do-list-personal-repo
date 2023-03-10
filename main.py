57e26edaac2c96f67a411a03b200ed6eb199a7f7
keiransystem14 <keiran1412@hotmail.com>
2023-03-10 05:21:25 +0000

    Used an image of the topbar from internet images and modified the main.py file to add the topbar image into the application

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 
 from tkinter import *
 
-window=Tk()
-window.title("To-Do-List")
-window.geometry("400x650+400+100")
+app=Tk()
+app.title("To-Do-List")
+app.geometry("400x650+400+100")
 
 tasklist = []
 
+#to-do-list application icon
 
+Image_logo=PhotoImage(file="Image_Object/sketch-3-todo-list-app-icon-template.png")
+app.iconphoto(False, Image_logo)
 
-window.mainloop()
+#to-do-list application top bar
+
+TopImage=PhotoImage(file="Image_Object/topbar.png")
+
+Label(app,image=TopImage).pack()
+
+app.mainloop()
 
