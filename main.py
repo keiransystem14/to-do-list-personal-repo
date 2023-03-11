b708cad6aec4ff39d910611a4738004199b34191
keiransystem14 <keiran1412@hotmail.com>
2023-03-11 11:37:29 +0000

    Adding a text box for me to enter the task on there

--- main.py
+++ main.py
@@ -23,12 +23,20 @@ Label(app,image=dockImage,bg="#32405b").place(x=30,y=25)
 
 #Adding the logo onto the to-do-list application
 TaskImage=PhotoImage(file="Image_Object/task.png")
-Label(app,image=TaskImage).place(x=30,y=25)
+Label(app,image=TaskImage,bg="#32405b").place(x=340,y=25)
 
 #Creating a title for the to-do-list application
 title=Label(app, text="TASK LIST",font="arial 20 bold",fg="white",bg="#32405b")
 title.place(x=130,y=20)
 
+#list (main)
+
+textbox= Frame(app,width=400,height=50,bg="white", bd=0)
+textbox.place(x=0,y=180)
+
+write=StringVar()
+write_entry=Entry(textbox,width=18,font="arial 20")
+write_entry.place(x=10,y=7)
 
 app.mainloop()
 
