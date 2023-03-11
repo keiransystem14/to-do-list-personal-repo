d76e18ff67e55b669de92ac6746bdaa9af5c40f5
keiransystem14 <keiran1412@hotmail.com>
2023-03-11 16:27:44 +0000

    Adding delete button and changing the images because I want to customize my personal to-do-list application

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ Label(app,image=TopImage).pack()
 
 #to-do-list dock image
 dockImage=PhotoImage(file="Image_Object/dock.png")
-Label(app,image=dockImage,bg="#32405b").place(x=30,y=25)
+Label(app,image=dockImage,bg="#CCFFAB").place(x=30,y=20)
 
 #Adding the logo onto the to-do-list application
 TaskImage=PhotoImage(file="Image_Object/task.png")
-Label(app,image=TaskImage,bg="#32405b").place(x=340,y=25)
+Label(app,image=TaskImage,bg="#CCFFAB").place(x=340,y=20)
 
 #Creating a title for the to-do-list application
-title=Label(app, text="TASK LIST",font="arial 20 bold",fg="white",bg="#32405b")
-title.place(x=130,y=20)
+title=Label(app, text="TASK LIST",font="arial 20 bold",fg="black",bg="#CCFFAB")
+title.place(x=130,y=15)
 
 #Creating a text box
 
 textbox= Frame(app,width=400,height=43,bg="white", bd=0)
-textbox.place(x=0,y=180)
+textbox.place(x=0,y=140)
 
 write=StringVar()
 write_entry=Entry(textbox,width=20,font="arial 18")
@@ -41,14 +41,14 @@ write_entry.place(x=10,y=5)
 #Creating a button on this application
 
 button=Button(app,text="ADD",font="arial 20",bd=0, width=6, bg="#5a95ff", fg="#fff")
-button.place(x=300,y=180)
+button.place(x=300,y=140)
 
 #Creating a listbox to store the list 
 
-padding = Frame(app,bd=3,width=700,height=280,bg="#32405b")
-padding.pack(pady=(160,0))
+padding = Frame(app,bd=3,width=700,height=280,bg="#CCFFAB")
+padding.pack(pady=(120,0))
 
-listbox = Listbox(padding,font=('arial',12),width=40,height=16,bg="#32405b", bd=0, fg="white", cursor="hand2", selectbackground="#5a95ff")
+listbox = Listbox(padding,font=('arial',12),width=40,height=16,bg="#CCFFAB", bd=0, fg="black", cursor="hand2", selectbackground="#CCFFAB")
 listbox.pack(side=LEFT, fill=BOTH, padx=2)
 scrollbar = Scrollbar(padding)
 scrollbar.pack(side= RIGHT, fill=BOTH)
