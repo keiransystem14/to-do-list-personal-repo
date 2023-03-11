f82dca46a00494bac2aa5f0d2b86d663b04578fe
keiransystem14 <keiran1412@hotmail.com>
2023-03-11 08:40:32 +0000

    Adding dock image and coding it to ensure it uses the dock image

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ app.iconphoto(False, Image_logo)
 #to-do-list application top bar
 
 TopImage=PhotoImage(file="Image_Object/topbar.png")
-
 Label(app,image=TopImage).pack()
+
+#to-do-list dock image
+dockImage=PhotoImage(file="Image_Object/dock.png")
+Label(app,image=dockImage,bg="#32405b").place(x=30,y=25)
+
 
 app.mainloop()
 
