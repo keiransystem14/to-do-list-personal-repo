608896391c8dcb0ed0de05af26907093032c74e0
keiransystem14 <keiran1412@hotmail.com>
2023-03-11 09:46:18 +0000

    Changing the icon and adding the title of the to-do-list application

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ tasklist = []
 
 #to-do-list application icon
 
-Image_logo=PhotoImage(file="Image_Object/sketch-3-todo-list-app-icon-template.png")
+Image_logo=PhotoImage(file="Image_Object/task.png")
 app.iconphoto(False, Image_logo)
 
 #to-do-list application top bar
@@ -20,6 +20,14 @@ Label(app,image=TopImage).pack()
 #to-do-list dock image
 dockImage=PhotoImage(file="Image_Object/dock.png")
 Label(app,image=dockImage,bg="#32405b").place(x=30,y=25)
+
+#Adding the logo onto the to-do-list application
+TaskImage=PhotoImage(file="Image_Object/task.png")
+Label(app,image=TaskImage).place(x=30,y=25)
+
+#Creating a title for the to-do-list application
+title=Label(app, text="TASK LIST",font="arial 20 bold",fg="white",bg="#32405b")
+title.place(x=130,y=20)
 
 
 app.mainloop()
