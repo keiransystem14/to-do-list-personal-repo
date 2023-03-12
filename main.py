38b44758c449002ce6e534e2e4cf8c493456b8e0
keiransystem14 <keiran1412@hotmail.com>
2023-03-12 09:04:31 +0000

    Testing that the add button works and I was able to add to add my task from the single line textbox into the listbox

--- main.py
+++ main.py
@@ -5,22 +5,47 @@ app=Tk()
 app.title("To-Do-List")
 app.geometry("400x650+400+100")
 
+#array
 workload_list = []
 
 
+#This means it defines a function called addworkload(), workload vairable is used to get information that was entered onto the single line textbox. write_entry.delete function is used to clear the the information entered on the signle line textbox after the add button is pressed. 
+
+#'if workload' statement means if information is entered on the single line textbox on the application, it opens the file called workload.txt and writes the item from the single line textbox. It then appends the item from the new line onto the array called workload_list.
+
+def addWorkload():
+    workload = write_entry.get()
+    write_entry.delete(0, END)
+
+    if workload:
+        with open("workload.txt","a") as taskfile:
+            taskfile.write(f"\n{workload}")
+        workload_list.append(workload)
+        listbox.insert(END, workload)
+        
+
+
 def WorkloadFile():
-    with open("workload.txt","r") as taskfile:               #With open function means it tells the computer to open and read the workload.txt file
-        workloads = taskfile.readlines()                     #Readline method means it returns one line from the file.
+
+    try:
+
+        global workload_list
+
+        with open("workload.txt","r") as taskfile:               #With open function means it tells the computer to open and read the workload.txt file
+            workloads = taskfile.readlines()                     #Readline method means it returns one line from the file.
 
 
 # In this code, python for loop is used to run it's block of code multiple times until it reaches and reads the last item.In this example below, the variable workload tries to access each item inside the workloads text file on an iterated loop. !='\'n' means it's evaluated as true if the last item is different to a new line last item is encountered. When the new line which appears to be the last line of the text file, it gets added onto the workload_list array. It adds the workload_list array onto the listbox. 
 
 
-    for workload in workloads:                             
-        if workload !='\n':
-            workload_list.append(workload)
-            listbox.insert(END, workload)
+        for workload in workloads:                             
+            if workload !='\n':
+                workload_list.append(workload)
+                listbox.insert(END, workload)
             
+    except:
+        file= open('workload.txt','w')
+        file.close()
 
 #to-do-list application icon
 
@@ -53,9 +78,9 @@ write=StringVar()
 write_entry=Entry(textbox,width=20,font="arial 18")
 write_entry.place(x=10,y=5)
 
-#Creating a button on this application
+#Creating a button on this application. The add button uses the addworkload function to perform the following actions after entering words into the single line textbox.
 
-button=Button(app,text="ADD",font="arial 20",bd=0, width=6, bg="#5a95ff", fg="#fff")
+button=Button(app,text="ADD",font="arial 20",bd=0, width=6, bg="#5a95ff", fg="#fff", command=addWorkload)
 button.place(x=300,y=140)
 
 #Creating a listbox to store the list 
